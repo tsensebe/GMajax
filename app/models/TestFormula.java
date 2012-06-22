342096bbf1c705c1cdab7307a7d107caa93bd860
ni <thomas.sensebe@gmail.com>
2012-06-22 15:55:11 +0200

    add form for testing

---
+++ app/models/TestFormula.java
@@ -0,0 +1,20 @@
+package models;
+
+import java.math.BigDecimal;
+
+public class TestFormula {
+
+	public Integer lvl;
+	public Integer str;
+	public Integer dex;
+	public Integer intel;
+	public Integer vita;
+	public BigDecimal bonusLife;
+	public Integer armor;
+	public Integer dmgMin;
+	public Integer dmgMax;
+	public BigDecimal attSpd;
+	public BigDecimal attSpdBonus;
+	public Integer resistBonus;
+	
+}
